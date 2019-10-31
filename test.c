5e27e3231f122edfb1323fead2013b1aa86af42a
DH <dhlife09@gmail.com>
2019-10-31 23:56:03 +0900

    Update test.c

--- test.c
+++ test.c
@@ -1,92 +1,68 @@
+#include "src/lea.h"
+#include "src/lea_locl.h"
+
+#include "benchmark.h"
+#include "lea_benchmark.h"
+#include "lea_vs.h"
+
 #include <stdio.h>
-#include <stdlib.h>
- 
-#include <string>
- 
-#include "include\config.h"
-#include "include\lea.h"
- 
-#include "KISA_SHA256.h"
- 
- 
-unsigned char mk[32] = { 0, };
- 
-unsigned int mk_len = 32;
- 
-char ctr_enc[16] = { 0, };
- 
-unsigned char pyn_buffer[16] = { 0, };
-//unsigned int pyn_buffer_len = 16;
- 
-char cyp_buffer[16];
-//unsigned int cyp_buffer_len = 16;
- 
-int main()
+
+#include "src/lea.h"
+#include "src/lea_locl.h"
+
+#include "benchmark.h"
+#include "lea_benchmark.h"
+#include "lea_vs.h"
+
+#include <stdio.h>
+
+/*
+입력된 마스터키를 이용하여 라운드 키를 계산하고 LEA_KEY 구조체를 생성한다.
+LEA_KEY 구조체는 마스터키 길이 정보와 라운드 키를 포함한다. LEA_KEY 구조체는 사용자가 선언한 후 입력해야 하며
+초기화하지 않은 구조체의 주소를 입력해도 정상 동작한다.
+*/
+
+void lea_set_key(	// LEA 라운드 키 생성
+	LEA_KEY* key,		//LEA 라운드 키와 라운드 수 정보를 가진 LEA_KEY 구조체의 구조
+	const unsigned char* mk,	// 마스터 키
+	unsigned int mk_len		// 마스터키의 길이 (입력 값으로 16, 24, 32만 가능)
+);
+
+/*
+입력된 평문을 ECB 모드를 이용해 암호화한다.
+*/
+
+void lea_ecb_enc(		//입력된 평문을 ECB 모드로 암호화d
+	unsigned char* ct,		//암호문
+	const unsigned char* pt,		//암호화하려는 평문
+	unsigned int pt_len,		//평문의 길이 (16의 배수 길이만 입력가능)
+	const LEA_KEY* key		//lea_set_key 함수를 통해 설정된 LEA_KEY 구조체의 주소
+	ct out[]
+);
+
+
+int main(void)
 {
-    size_t M = 0;
- 
-    SHA256_INFO sha256info;
- 
-    LEA_KEY key;
- 
-    FILE *fip = NULL;
-    FILE *fop = NULL;
- 
-    //fip = fopen64("test", "rb");
-    //fop = fopen64("test.LEA_CTR", "wb");
- 
-    fopen_s(&fip, "test", "rb");
-    fopen_s(&fop, "test.lea", "wb");
- 
-    if (fip == NULL)
-    {
-        return -1;
-    }
- 
-    if (fop == NULL)
-    {
-        return -1;
-    }
-    
-    char password[256]="TestMessage";
- 
-    SHA256_Init(&sha256info);
-    SHA256_Process(&sha256info, (unsigned char*)password, strlen(password));
- 
-    SHA256_Close(&sha256info, mk);
- 
-    lea_set_key(&key, mk, mk_len);
- 
-    M = fread(pyn_buffer, sizeof(char), sizeof(pyn_buffer), fip);
-    lea_ecb_enc((unsigned char*)cyp_buffer, pyn_buffer, M, &key);
- 
-    if (fwrite(cyp_buffer, sizeof(char), M, fop) == 0)
-    {
-        fclose(fip);
-        fclose(fop);
-        _unlink("test.lea"); // 에러난 파일 지우고 종료
-        return -1;
-    }
- 
-    strcpy(ctr_enc, cyp_buffer);
- 
-    while ((M = fread(pyn_buffer, sizeof(char), sizeof(pyn_buffer), fip)) != NULL)
-    {
-        lea_ctr_enc((unsigned char*) cyp_buffer, pyn_buffer, M, (unsigned char*)ctr_enc, &key);
-        //lea_ctr_dec(cyp_buffer, pyn_buffer,M,ctr_enc,&key);
-        if (fwrite(cyp_buffer, sizeof(char), M, fop) == 0)
-        {
-            fclose(fip);
-            fclose(fop);
-            _unlink("test.lea"); // 에러난 파일 지우고 종료
-            return -1;
-        }
-        //lea_ctr_enc(cyp_buffer, pyn_buffer, pyn_buffer_len, ctr_enc, &key);
-        //fwrite(cyp_buffer, cyp_buffer_len, 1, fop);
-    }
- 
-    fclose(fip);
-    fclose(fop);
- 
-    return 0;
+	int ret;
+	printf("SIMD: %s\n", get_simd_type());
+	if (ret = lea_mmt_ecb_test())	printf("LEA ECB FAIL(%d)\n", ret);
+	if (ret = lea_mmt_cbc_test())	printf("LEA CBC FAIL(%d)\n", ret);
+	if (ret = lea_mmt_ctr_test())	printf("LEA CTR FAIL(%d)\n", ret);
+	if (ret = lea_mmt_ofb_test())	printf("LEA OFB FAIL(%d)\n", ret);
+	if (ret = lea_mmt_cfb_test())	printf("LEA CFB FAIL(%d)\n", ret);
+	if (ret = lea_cmac_g_test())		printf("LEA CMAC FAIL(%d)\n", ret);
+	if (ret = lea_ccm_ge_test())		printf("LEA CCM FAIL(%d)\n", ret);
+	if (ret = lea_gcm_ae_test())		printf("LEA GCM FAIL(%d)\n", ret);
+
+	lea_key_benchmark();
+	lea_ecb_benchmark();
+	lea_cbc_benchmark();
+	lea_ctr_benchmark();
+	lea_ofb_benchmark();
+	lea_cfb_benchmark();
+	lea_cmac_benchmark();
+	lea_ccm_benchmark();
+	lea_gcm_benchmark();
+
+	return 0;
 }
